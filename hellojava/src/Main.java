062f2296189dacd13c03396af091ea0379c4e237
odraude222k <160641722+odraude222k@users.noreply.github.com>
2024-02-20 14:07:20 -0300

    código hello java

---
+++ hellojava/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 20;
+        System.out.println("Olá Java");
+        System.out.println(x+y);
+        System.out.println("A soma de x e y é: " + (x+y));
+    }
+}
