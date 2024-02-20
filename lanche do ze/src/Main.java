2ae3505d56b096e13b3418faa61906d9ff2c141e
odraude222k <160641722+odraude222k@users.noreply.github.com>
2024-02-20 14:38:37 -0300

    lanche do zé

---
+++ lanche do ze/src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int lanche1 = 10;
+        int lanche2 = 4;
+        int lanche3 = 2;
+        int soma;
+        float media;
+
+        soma = lanche1+lanche2+lanche3;
+
+        media = soma/3.0f; // colocar f caso seja float
+
+
+        System.out.println("O total de lanches foi: " +soma);
+        System.out.println("A media de lanches foi: " +media);
+
+    }
+}
